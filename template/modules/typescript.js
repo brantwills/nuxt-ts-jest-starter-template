5b9836db225fb4c9a8c94c7d5b2de565ff1f9742
Brant Wills <bwills@discovertec.com>
2018-01-12 13:33:50 -0500

    chore: adding typescript extension in modules

--- template/modules/typescript.js
+++ template/modules/typescript.js
@@ -1,4 +1,7 @@
 module.exports = function (options) {
+  // Add .ts extension for store, middleware and more
+  this.nuxt.options.extensions.push('ts')
+
   // Extend build
   this.extendBuild(config => {
     const tsLoader = {
